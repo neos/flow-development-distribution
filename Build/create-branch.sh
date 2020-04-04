5fc8a93bec1a8d61ebd59bd9e5b452cc0cb9634e
Alexander Berl <a.berl@outlook.com>
2020-04-04 20:56:26 +0200

    TASK: Remove composer 1.5.2 hardcoded version workaround

--- Build/create-branch.sh
+++ Build/create-branch.sh
@@ -14,10 +14,6 @@ set -e
 if [ -z "$BRANCH" ]; then echo "\$BRANCH not set"; exit 1; fi
 if [ -z "$BUILD_URL" ]; then echo "\$BUILD_URL not set"; exit 1; fi
 
-if [ ! -e "composer-1.5.2.phar" ]; then
-    curl -L https://github.com/composer/composer/releases/download/1.5.2/composer.phar > composer-1.5.2.phar
-fi
-
 if [ ! -e "composer.phar" ]; then
     ln -s /usr/local/bin/composer.phar composer.phar
 fi
@@ -46,10 +42,6 @@ push_branch ${BRANCH} "Packages/Framework"
 cd Packages/Application/Neos.Welcome && git checkout -b ${BRANCH} origin/master ; cd -
 push_branch ${BRANCH} "Packages/Application/Neos.Welcome"
 
-# use old composer.phar to work around https://github.com/composer/composer/issues/7800
-rm composer.phar
-ln -s composer-1.5.2.phar composer.phar
-
 $(dirname ${BASH_SOURCE[0]})/set-dependencies.sh "${BRANCH}.x-dev" ${BRANCH} "${BUILD_URL}" || exit 1
 
 push_branch ${BRANCH} "Distribution"
@@ -71,7 +63,3 @@ php ./composer.phar --working-dir=Distribution require --no-update "neos/flow-de
 $(dirname ${BASH_SOURCE[0]})/set-dependencies.sh "${BRANCH}.x-dev" ${BRANCH} "${BUILD_URL}" || exit 1
 
 push_branch ${BRANCH} "Distribution"
-
-# undo "use old composer.phar to work around https://github.com/composer/composer/issues/7800"
-rm composer.phar
-ln -s /usr/local/bin/composer.phar composer.phar
