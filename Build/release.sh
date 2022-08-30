83b36b68a153705c171c20279c0b511196ca51d5
Karsten Dambekalns <karsten@dambekalns.de>
2022-08-30 08:42:07 +0200

    TASK: Tag more packages for *.0 release
    Tag the following for a new major release, too:
    
    - Neos.Welcome
    - Neos.Behat
    - BuildEssentials
    
    Fixes #81

--- Build/release.sh
+++ Build/release.sh
@@ -49,3 +49,13 @@ Build/tag-release.sh "${VERSION}" "${BRANCH}" "${BUILD_URL}"
 EXTENDED_RELEASE_NOTES="${RELEASE_NOTES}\n\nSee [changelog](http://flowframework.readthedocs.io/en/${BRANCH}/TheDefinitiveGuide/PartV/ChangeLogs/${VERSION//.}.html) for details."
 API_JSON=$(printf '{"tag_name": "%s","name": "Flow %s","body": "%s","draft": false,"prerelease": false}' "${VERSION}" "${VERSION}" "${EXTENDED_RELEASE_NOTES}")
 curl -H "Authorization: token ${GITHUB_TOKEN}" --data "${API_JSON}" https://api.github.com/repos/neos/flow-development-collection/releases
+
+if [[ "$VERSION" == *.0 ]]; then
+  API_JSON=$(printf '{"tag_name": "%s","name": "%s","body": "%s", "draft": false,"prerelease": false}' "${VERSION}" "${VERSION}" "Released through ${BUILD_URL}")
+  # release Neos.Behat *.0
+  curl -H "Authorization: token ${GITHUB_TOKEN}" --data "${API_JSON}" https://api.github.com/repos/neos/behat/releases
+  # release BuildEssentials *.0
+  curl -H "Authorization: token ${GITHUB_TOKEN}" --data "${API_JSON}" https://api.github.com/repos/neos/BuildEssentials/releases
+  # release Neos.Welcome *.0
+  curl -H "Authorization: token ${GITHUB_TOKEN}" --data "${API_JSON}" https://api.github.com/repos/neos/flow-welcome/releases
+fi
