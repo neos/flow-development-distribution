8e467afdd8723f9c62ccadb1dde19fd263b08aec
Karsten Dambekalns <karsten@dambekalns.de>
2025-03-04 08:32:07 +0100

    BUGFIX: Fix installation of composer for build

--- Build/release.sh
+++ Build/release.sh
@@ -32,7 +32,20 @@ rm -rf Distribution
 git clone -b "${BRANCH}" git@github.com:neos/flow-base-distribution.git Distribution
 
 if [ ! -e "composer.phar" ]; then
-  ln -s /usr/local/bin/composer.phar composer.phar
+  EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+  if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+  then
+      >&2 echo 'ERROR: Invalid installer checksum'
+      rm composer-setup.php
+      exit 1
+  fi
+
+  php composer-setup.php --quiet
+  RESULT=$?
+  rm composer-setup.php
 fi
 
 composer.phar -v update
