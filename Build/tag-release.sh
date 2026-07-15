neos/flow-development-distribution

commit 075b90df690c1612eeb5fc933451f896f03c6798

TASK: Some code (style) tweaks

Build/tag-release.sh
@@ -12,51 +12,53 @@
 # BUILD_URL        used in commit message
 #
 
-source $(dirname ${BASH_SOURCE[0]})/BuildEssentials/ReleaseHelpers.sh
+source "$(dirname "${BASH_SOURCE[0]}")/BuildEssentials/ReleaseHelpers.sh"
 
-if [ -z "$1" ] ; then
-	echo >&2 "No version specified (e.g. 2.1.*) as first parameter"
-	exit 1
+if [ -z "$1" ]; then
+  echo >&2 "No version specified (e.g. 2.1.*) as first parameter"
+  exit 1
 fi
 VERSION=$1
 
-if [ -z "$2" ] ; then
-	echo >&2 "No branch specified (e.g. 2.1) as second parameter"
-	exit 1
+if [ -z "$2" ]; then
+  echo >&2 "No branch specified (e.g. 2.1) as second parameter"
+  exit 1
 fi
 BRANCH=$2
 
-if [ -z "$3" ] ; then
-	echo >&2 "No build URL specified as third parameter"
-	exit 1
+if [ -z "$3" ]; then
+  echo >&2 "No build URL specified as third parameter"
+  exit 1
 fi
 BUILD_URL=$3
 
-if [ ! -d "Distribution" ]; then echo '"Distribution" folder not found. Clone the base distribution into "Distribution"'; exit 1; fi
+if [ ! -d "Distribution" ]; then
+  echo '"Distribution" folder not found. Clone the base distribution into "Distribution"'
+  exit 1
+fi
 
-$(dirname ${BASH_SOURCE[0]})/set-dependencies.sh ${VERSION} ${BRANCH} "${BUILD_URL}" || exit 1
+"$(dirname "${BASH_SOURCE[0]}")/set-dependencies.sh" "${VERSION}" "${BRANCH}" "${BUILD_URL}" || exit 1
 
 echo "Tagging distribution"
-tag_version ${VERSION} ${BRANCH} "${BUILD_URL}" "Distribution"
-push_branch ${BRANCH} "Distribution"
-push_tag ${VERSION} "Distribution"
+tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Distribution"
+push_branch "${BRANCH}" "Distribution"
+push_tag "${VERSION}" "Distribution"
 
 echo "Tagging development collection"
-tag_version ${VERSION} ${BRANCH} "${BUILD_URL}" "Packages/Framework"
-push_branch ${BRANCH} "Packages/Framework"
-push_tag ${VERSION} "Packages/Framework"
+tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Packages/Framework"
+push_branch "${BRANCH}" "Packages/Framework"
+push_tag "${VERSION}" "Packages/Framework"
 
-if [[ "$VERSION" == *.0 ]]
-then
-  echo "Tagging buildessentials"
-  tag_version ${VERSION} ${BRANCH} "${BUILD_URL}" "Build/BuildEssentials"
-  push_tag ${VERSION} "Build/BuildEssentials"
+if [[ "${VERSION}" == "*.0" ]]; then
+  echo "Tagging BuildEssentials"
+  tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Build/BuildEssentials"
+  push_tag "${VERSION}" "Build/BuildEssentials"
 
-  echo "Tagging behat"
-  tag_version ${VERSION} ${BRANCH} "${BUILD_URL}" "Packages/Application/Neos.Behat"
-  push_tag ${VERSION} "Packages/Application/Neos.Behat"
+  echo "Tagging Behat"
+  tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Packages/Application/Neos.Behat"
+  push_tag "${VERSION}" "Packages/Application/Neos.Behat"
 
-  echo "Tagging welcome"
-  tag_version ${VERSION} ${BRANCH} "${BUILD_URL}" "Packages/Application/Neos.Welcome"
-  push_tag ${VERSION} "Packages/Application/Neos.Welcome"
+  echo "Tagging Welcome"
+  tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Packages/Application/Neos.Welcome"
+  push_tag "${VERSION}" "Packages/Application/Neos.Welcome"
 fi

Build/create-branch.sh
@@ -11,50 +11,60 @@
 
 set -e
 
-if [ -z "$BRANCH" ]; then echo "\$BRANCH not set"; exit 1; fi
-if [ -z "$BUILD_URL" ]; then echo "\$BUILD_URL not set"; exit 1; fi
+if [ -z "${BRANCH}" ]; then
+  echo "\$BRANCH not set"
+  exit 1
+fi
+if [ -z "$BUILD_URL" ]; then
+  echo "\$BUILD_URL not set"
+  exit 1
+fi
 
 if [ ! -e "composer.phar" ]; then
-    ln -s /usr/local/bin/composer.phar composer.phar
+  ln -s /usr/local/bin/composer.phar composer.phar
 fi
 
 php ./composer.phar -v update
 
-source $(dirname ${BASH_SOURCE[0]})/BuildEssentials/ReleaseHelpers.sh
+source "$(dirname "${BASH_SOURCE[0]}")/BuildEssentials/ReleaseHelpers.sh"
 
 # start with Base Distribution
 rm -rf Distribution
 git clone git@github.com:neos/flow-base-distribution.git Distribution
 
 # branch distribution
-cd Distribution && git checkout -b ${BRANCH} origin/master ; cd -
-push_branch ${BRANCH} "Distribution"
+cd Distribution && git checkout -b "${BRANCH}" origin/master
+cd -
+push_branch "${BRANCH}" "Distribution"
 
 # branch BuildEssentials
-cd Build/BuildEssentials && git checkout -b ${BRANCH} origin/master ; cd -
-push_branch ${BRANCH} "Build/BuildEssentials"
+cd Build/BuildEssentials && git checkout -b "${BRANCH}" origin/master
+cd -
+push_branch "${BRANCH}" "Build/BuildEssentials"
 
 # branch development collection
-cd Packages/Framework && git checkout -b ${BRANCH} origin/master ; cd -
-push_branch ${BRANCH} "Packages/Framework"
+cd Packages/Framework && git checkout -b "${BRANCH}" origin/master
+cd -
+push_branch "${BRANCH}" "Packages/Framework"
 
 # branch welcome package
-cd Packages/Application/Neos.Welcome && git checkout -b ${BRANCH} origin/master ; cd -
-push_branch ${BRANCH} "Packages/Application/Neos.Welcome"
+cd Packages/Application/Neos.Welcome && git checkout -b "${BRANCH}" origin/master
+cd -
+push_branch "${BRANCH}" "Packages/Application/Neos.Welcome"
 
 # branch behat package
 cd Packages/Application/Neos.Behat && git checkout -b ${BRANCH} origin/master ; cd -
 # special procedure for updating the composer.lock of Behat setup - see https://github.com/neos/behat/issues/23
 cd Packages/Application/Neos.Behat/Resources/Private/Build/Behat && composer update --no-install && git add composer.lock && git commit -m "TASK: Update composer.lock" ; cd -
-push_branch ${BRANCH} "Packages/Application/Neos.Behat"
+push_branch "${BRANCH}" "Packages/Application/Neos.Behat"
 
-$(dirname ${BASH_SOURCE[0]})/set-dependencies.sh "${BRANCH}.x-dev" ${BRANCH} "${BUILD_URL}" || exit 1
+"$(dirname "${BASH_SOURCE[0]}")/set-dependencies.sh" "${BRANCH}.x-dev" "${BRANCH}" "${BUILD_URL}" || exit 1
 
-push_branch ${BRANCH} "Distribution"
-push_branch ${BRANCH} "Build/BuildEssentials"
-push_branch ${BRANCH} "Packages/Framework"
-push_branch ${BRANCH} "Packages/Application/Neos.Welcome"
-push_branch ${BRANCH} "Packages/Application/Neos.Behat"
+push_branch "${BRANCH}" "Distribution"
+push_branch "${BRANCH}" "Build/BuildEssentials"
+push_branch "${BRANCH}" "Packages/Framework"
+push_branch "${BRANCH}" "Packages/Application/Neos.Welcome"
+push_branch "${BRANCH}" "Packages/Application/Neos.Behat"
 
 # same procedure again with the Development Distribution
 
@@ -62,11 +72,12 @@ rm -rf Distribution
 git clone git@github.com:neos/flow-development-distribution.git Distribution
 
 # branch distribution
-cd Distribution && git checkout -b ${BRANCH} origin/master ; cd -
-push_branch ${BRANCH} "Distribution"
+cd Distribution && git checkout -b "${BRANCH}" origin/master
+cd -
+push_branch "${BRANCH}" "Distribution"
 
 # special case for the Development Distribution
 php ./composer.phar --working-dir=Distribution require --no-update "neos/flow-development-collection:${BRANCH}.x-dev"
-$(dirname ${BASH_SOURCE[0]})/set-dependencies.sh "${BRANCH}.x-dev" ${BRANCH} "${BUILD_URL}" || exit 1
+"$(dirname "${BASH_SOURCE[0]}")/set-dependencies.sh" "${BRANCH}.x-dev" "${BRANCH}" "${BUILD_URL}" || exit 1
 
-push_branch ${BRANCH} "Distribution"
+push_branch "${BRANCH}" "Distribution"

Build/create-changelog.sh
@@ -12,8 +12,16 @@ set -e
 # GITHUB_TOKEN     to authenticate github calls and avoid API limits
 #
 
-if [ -z "$VERSION" ]; then echo "\$VERSION not set"; exit 1; fi
-if [ -z "$PREVIOUS_VERSION" ]; then echo "\$PREVIOUS_VERSION not set"; exit 1; fi
-export TARGET="Neos.Flow/Documentation/TheDefinitiveGuide/PartV/ChangeLogs/$(echo ${VERSION} | tr -d .).rst"
+if [ -z "$VERSION" ]; then
+  echo "\$VERSION not set"
+  exit 1
+fi
+if [ -z "$PREVIOUS_VERSION" ]; then
+  echo "\$PREVIOUS_VERSION not set"
+  exit 1
+fi
 
-php Build/BuildEssentials/build-tools.php neos:create-changelog flow $PREVIOUS_VERSION $VERSION $TARGET --githubToken=$GITHUB_TOKEN --buildUrl=$BUILD_URL
+TARGET="Neos.Flow/Documentation/TheDefinitiveGuide/PartV/ChangeLogs/$(echo "${VERSION}" | tr -d .).rst"
+export TARGET
+
+php Build/BuildEssentials/build-tools.php neos:create-changelog flow "$PREVIOUS_VERSION" "$VERSION" "$TARGET" --githubToken="$GITHUB_TOKEN" --buildUrl="$BUILD_URL"

Build/create-releasenotes.sh
@@ -12,8 +12,15 @@ set -e
 # GITHUB_TOKEN     to authenticate github calls and avoid API limits
 #
 
-if [ -z "$VERSION" ]; then echo "\$VERSION not set"; exit 1; fi
-if [ -z "$PREVIOUS_VERSION" ]; then echo "\$PREVIOUS_VERSION not set"; exit 1; fi
-export TARGET="Neos.Flow/Documentation/TheDefinitiveGuide/PartV/ReleaseNotes/$(echo ${VERSION} | tr -d .).rst"
+if [ -z "$VERSION" ]; then
+  echo "\$VERSION not set"
+  exit 1
+fi
+if [ -z "$PREVIOUS_VERSION" ]; then
+  echo "\$PREVIOUS_VERSION not set"
+  exit 1
+fi
+TARGET="Neos.Flow/Documentation/TheDefinitiveGuide/PartV/ReleaseNotes/$(echo "${VERSION}" | tr -d .).rst"
+export TARGET
 
-php Build/BuildEssentials/build-tools.php neos:create-releasenotes flow $PREVIOUS_VERSION $VERSION $TARGET --filter=FEATURE --githubToken=$GITHUB_TOKEN --buildUrl=$BUILD_URL
+php Build/BuildEssentials/build-tools.php neos:create-releasenotes flow "$PREVIOUS_VERSION" "$VERSION" "$TARGET" --filter=FEATURE --githubToken="$GITHUB_TOKEN" --buildUrl="$BUILD_URL"

Build/release.sh
@@ -11,24 +11,36 @@
 # BUILD_URL        used in commit message
 #
 
-if [ -z "$VERSION" ]; then echo "\$VERSION not set"; exit 1; fi
-if [ -z "$PREVIOUS_VERSION" ]; then echo "\$PREVIOUS_VERSION not set"; exit 1; fi
-if [ -z "$BRANCH" ]; then echo "\$BRANCH not set"; exit 1; fi
-if [ -z "$BUILD_URL" ]; then echo "\$BUILD_URL not set"; exit 1; fi
+if [ -z "$VERSION" ]; then
+  echo "\$VERSION not set"
+  exit 1
+fi
+if [ -z "$PREVIOUS_VERSION" ]; then
+  echo "\$PREVIOUS_VERSION not set"
+  exit 1
+fi
+if [ -z "$BRANCH" ]; then
+  echo "\$BRANCH not set"
+  exit 1
+fi
+if [ -z "$BUILD_URL" ]; then
+  echo "\$BUILD_URL not set"
+  exit 1
+fi
 
 rm -rf Distribution
-git clone -b ${BRANCH} git@github.com:neos/flow-base-distribution.git Distribution
+git clone -b "${BRANCH}" git@github.com:neos/flow-base-distribution.git Distribution
 
 if [ ! -e "composer.phar" ]; then
-    ln -s /usr/local/bin/composer.phar composer.phar
+  ln -s /usr/local/bin/composer.phar composer.phar
 fi
 
 composer.phar -v update
 Build/create-changelog.sh
-if [[ "$VERSION" == *.0 ]]
+if [[ "$VERSION" == *.0 ]]; then
   Build/create-releasenotes.sh
 fi
-Build/tag-release.sh ${VERSION} ${BRANCH} ${BUILD_URL}
+Build/tag-release.sh "${VERSION}" "${BRANCH}" "${BUILD_URL}"
 
 #
 # Create a new "Release" on Github:

Build/set-dependencies.sh
@@ -11,44 +11,47 @@
 # BUILD_URL        used in commit message
 #
 
-source $(dirname ${BASH_SOURCE[0]})/BuildEssentials/ReleaseHelpers.sh
+source "$(dirname "${BASH_SOURCE[0]}")/BuildEssentials/ReleaseHelpers.sh"
 
-COMPOSER_PHAR="$(dirname ${BASH_SOURCE[0]})/../composer.phar"
-if [ ! -f ${COMPOSER_PHAR} ]; then
-    echo >&2 "No composer.phar, expected it at ${COMPOSER_PHAR}"
-    exit 1
+COMPOSER_PHAR="$(dirname "${BASH_SOURCE[0]}")/../composer.phar"
+if [ ! -f "${COMPOSER_PHAR}" ]; then
+  echo >&2 "No composer.phar, expected it at ${COMPOSER_PHAR}"
+  exit 1
 fi
 
-if [ -z "$1" ] ; then
-    echo >&2 "No version specified (e.g. 2.1.*) as first parameter."
-    exit 1
+if [ -z "$1" ]; then
+  echo >&2 "No version specified (e.g. 2.1.*) as first parameter."
+  exit 1
 else
-    if [[ $1 =~ (dev)-.+ || $1 =~ .+(@dev|.x-dev) || $1 =~ (alpha|beta|RC|rc)[0-9]+ ]] ; then
-        VERSION=$1
-        STABILITY_FLAG=${BASH_REMATCH[1]}
+  if [[ $1 =~ (dev)-.+ || $1 =~ .+(@dev|.x-dev) || $1 =~ (alpha|beta|RC|rc)[0-9]+ ]]; then
+    VERSION=$1
+    STABILITY_FLAG=${BASH_REMATCH[1]}
+  else
+    if [[ $1 =~ ([0-9]+\.[0-9]+)\.[0-9] ]]; then
+      VERSION=~${BASH_REMATCH[1]}.0
     else
-        if [[ $1 =~ ([0-9]+\.[0-9]+)\.[0-9] ]] ; then
-            VERSION=~${BASH_REMATCH[1]}.0
-        else
-            echo >&2 "Version $1 could not be parsed."
-            exit 1
-        fi
+      echo >&2 "Version $1 could not be parsed."
+      exit 1
     fi
+  fi
 fi
 
-if [ -z "$2" ] ; then
-    echo >&2 "No branch specified (e.g. 2.1) as second parameter."
-    exit 1
+if [ -z "$2" ]; then
+  echo >&2 "No branch specified (e.g. 2.1) as second parameter."
+  exit 1
 fi
 BRANCH=$2
 
-if [ -z "$3" ] ; then
-    echo >&2 "No build URL specified as third parameter."
-    exit 1
+if [ -z "$3" ]; then
+  echo >&2 "No build URL specified as third parameter."
+  exit 1
 fi
 BUILD_URL="$3"
 
-if [ ! -d "Distribution" ]; then echo '"Distribution" folder not found. Clone the base distribution into "Distribution"'; exit 1; fi
+if [ ! -d "Distribution" ]; then
+  echo '"Distribution" folder not found. Clone the base distribution into "Distribution"'
+  exit 1
+fi
 
 echo "Setting distribution dependencies"
 
@@ -58,42 +61,42 @@ php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/welc
 php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/behat:${VERSION}"
 
 # Require exact versions of sub dependency packages, allowing unstable
-if [[ ${STABILITY_FLAG} ]] ; then
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/cache:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/eel:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/error-messages:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/flow-log:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-arrays:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-files:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-mediatypes:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-objecthandling:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-opcodecache:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-pdo:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-schema:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-unicode:${VERSION}"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/http-factories:${VERSION}"
+if [[ ${STABILITY_FLAG} ]]; then
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/cache:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/eel:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/error-messages:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/flow-log:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-arrays:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-files:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-mediatypes:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-objecthandling:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-opcodecache:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-pdo:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-schema:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/utility-unicode:${VERSION}"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution require --no-update "neos/http-factories:${VERSION}"
 # Remove dependencies not needed if releasing a stable version
 else
-    # Remove requirements for development version of sub dependency packages
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/cache"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/eel"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/error-messages"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/flow-log"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-arrays"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-files"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-mediatypes"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-objecthandling"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-opcodecache"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-pdo"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-schema"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-unicode"
-    php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/http-factories"
+  # Remove requirements for development version of sub dependency packages
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/cache"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/eel"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/error-messages"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/flow-log"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-arrays"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-files"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-mediatypes"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-objecthandling"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-opcodecache"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-pdo"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-schema"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/utility-unicode"
+  php "${COMPOSER_PHAR}" --working-dir=Distribution remove --no-update "neos/http-factories"
 fi
 
 php "${COMPOSER_PHAR}" --working-dir=Distribution require --dev --no-update "neos/kickstarter:${VERSION}"
 php "${COMPOSER_PHAR}" --working-dir=Distribution require --dev --no-update "neos/buildessentials:${VERSION}"
 
-commit_manifest_update ${BRANCH} "${BUILD_URL}" ${VERSION} "Distribution"
+commit_manifest_update "${BRANCH}" "${BUILD_URL}" "${VERSION}" "Distribution"
 
 echo "Setting packages dependencies"
 
@@ -111,8 +114,8 @@ php "${COMPOSER_PHAR}" --working-dir=Packages/Framework/Neos.Flow require --no-u
 php "${COMPOSER_PHAR}" --working-dir=Packages/Framework/Neos.Flow require --no-update "neos/utility-unicode:~${BRANCH}.0"
 php "${COMPOSER_PHAR}" --working-dir=Packages/Framework/Neos.Flow require --no-update "neos/http-factories:~${BRANCH}.0"
 
-for PACKAGE in Neos.Eel Neos.FluidAdaptor Neos.Kickstarter ; do
-    php "${COMPOSER_PHAR}" --working-dir=Packages/Framework/${PACKAGE} require --no-update "neos/flow:~${BRANCH}.0"
+for PACKAGE in Neos.Eel Neos.FluidAdaptor Neos.Kickstarter; do
+  php "${COMPOSER_PHAR}" --working-dir=Packages/Framework/${PACKAGE} require --no-update "neos/flow:~${BRANCH}.0"
 done
 
 php "${COMPOSER_PHAR}" --working-dir=Packages/Framework/Neos.Kickstarter require --no-update "neos/fluid-adaptor:~${BRANCH}.0"
@@ -120,6 +123,6 @@ php "${COMPOSER_PHAR}" --working-dir=Packages/Application/Neos.Welcome require -
 php "${COMPOSER_PHAR}" --working-dir=Packages/Application/Neos.Welcome require --no-update "neos/fluid-adaptor:~${BRANCH}.0"
 php "${COMPOSER_PHAR}" --working-dir=Packages/Application/Neos.Behat require --no-update "neos/flow:~${BRANCH}.0"
 
-commit_manifest_update ${BRANCH} "${BUILD_URL}" ${VERSION} "Packages/Framework"
-commit_manifest_update ${BRANCH} "${BUILD_URL}" ${VERSION} "Packages/Application/Neos.Behat"
-commit_manifest_update ${BRANCH} "${BUILD_URL}" ${VERSION} "Packages/Application/Neos.Welcome"
+commit_manifest_update "${BRANCH}" "${BUILD_URL}" "${VERSION}" "Packages/Framework"
+commit_manifest_update "${BRANCH}" "${BUILD_URL}" "${VERSION}" "Packages/Application/Neos.Behat"
+commit_manifest_update "${BRANCH}" "${BUILD_URL}" "${VERSION}" "Packages/Application/Neos.Welcome"

Build/update-references.sh
@@ -3,28 +3,33 @@
 # This is intended to be run on Jenkins, triggered by GitHub and will
 # update the references rendered from PHP sources.
 
-BRANCH=$(echo ${payload} | jq --raw-output '.ref | match("refs/heads/(.+)") | .captures | .[0].string')
+if [ -z "${payload}" ]; then
+  echo >&2 "No \$payload given"
+  exit 1
+fi
+
+BRANCH=$(echo "${payload}" | jq --raw-output '.ref | match("refs/heads/(.+)") | .captures | .[0].string')
 
 # reset distribution
 git reset --hard
-git checkout -B ${BRANCH} origin/${BRANCH}
-git reset --hard origin/${BRANCH}
+git checkout -B "${BRANCH}" "origin/${BRANCH}"
+git reset --hard "origin/${BRANCH}"
 
 # install dependencies
-php $(dirname ${BASH_SOURCE[0]})/../composer.phar update --no-interaction --no-progress --no-suggest
-php $(dirname ${BASH_SOURCE[0]})/../composer.phar require --no-interaction --no-progress neos/doctools
-php $(dirname ${BASH_SOURCE[0]})/../composer.phar require --no-interaction --no-progress neos/fluid-adaptor
+php "$(dirname "${BASH_SOURCE[0]}")/../composer.phar" update --no-interaction --no-progress --no-suggest
+php "$(dirname "${BASH_SOURCE[0]}")/../composer.phar" require --no-interaction --no-progress neos/doctools
+php "$(dirname "${BASH_SOURCE[0]}")/../composer.phar" require --no-interaction --no-progress neos/fluid-adaptor
 
 # render references
 ./flow cache:warmup
 ./flow reference:rendercollection Flow
 ./flow commandreference:rendercollection Flow
 
-cd Packages/Framework
+cd Packages/Framework || exit 1
 
 # reset changes only updating the generation date
-for unchanged in `git diff  --numstat | grep '1\t1\t' | cut -f3`; do
- git checkout -- $unchanged;
+for unchanged in $(git diff --numstat | grep '1\t1\t' | cut -f3); do
+  git checkout -- "${unchanged}"
 done
 
 # commit and push results to Framework dev collection
@@ -32,5 +37,5 @@ echo 'Commit and push to Framework'
 git add Neos.Flow/Documentation/TheDefinitiveGuide/PartV
 git commit -m 'TASK: Update references [skip ci]'
 git config push.default simple
-git push origin ${BRANCH}
-cd -
+git push origin "${BRANCH}"
+cd - || exit 1

commit d967c6cb441c60d2ce303b6f767ed627dadabf89

TASK: Fix releasing build essentials, beta and welcome also for the first beta and first minor/minor
regression from https://github.com/neos/flow-development-distribution/commit/075b90df690c1612eeb5fc933451f896f03c6798 due to the quotes

Build/tag-release.sh
@@ -49,7 +49,7 @@ tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Packages/Framework"
 push_branch "${BRANCH}" "Packages/Framework"
 push_tag "${VERSION}" "Packages/Framework"
 
-if [[ "${VERSION}" == "*.0" ]]; then
+if [[ "${VERSION}" == *.0 || "${VERSION}" == *-beta1 ]]; then
   echo "Tagging BuildEssentials"
   tag_version "${VERSION}" "${BRANCH}" "${BUILD_URL}" "Build/BuildEssentials"
   push_tag "${VERSION}" "Build/BuildEssentials"